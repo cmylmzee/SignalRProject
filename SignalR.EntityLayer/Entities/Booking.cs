808fe9f107308435cd1ebcc4b6dcf08d536b09b4
Cem Yılmaz <yilmazcem1122@gmail.com>
2025-02-14 13:06:49 +0300

    Entities is ok

---
+++ SignalR.EntityLayer/Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Booking
+    {
+
+        public int BookingId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Phone {  get; set; } 
+
+        public string Mail { get; set; }
+
+        public int PersonCount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Category
+    {
+
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
