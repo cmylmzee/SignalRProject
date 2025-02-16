d0e2d332f63764c2318f341db8488c187de7b40d
Cem Yılmaz <yilmazcem1122@gmail.com>
2025-02-16 11:56:42 +0300

    Data Access Layer is completed

---
+++ SignalR.DataAccessLayer/EntityFramework/EfSocialMediaDal.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfSocialMediaDal : GenericRepository<SocialMedia>, ISocialMediaDal
+    {
+    }
+}
---
+++ SignalR.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class // Cloud işlemlerini tekrar etmemizi sağlayacak
+    {
+        private readonly SignalRContext context;
+        public GenericRepository(SignalRContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(T entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+            context.SaveChanges();  
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            context.Update(entity); 
+            context.SaveChanges();
+        }
+    }
+}
