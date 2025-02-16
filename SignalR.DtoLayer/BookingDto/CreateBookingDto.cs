6f6507a0572d73a647007c10f73022d808438711
Cem Yılmaz <yilmazcem1122@gmail.com>
2025-02-16 22:00:13 +0300

    DtoLayer Dto's is created

---
+++ SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.BookingDto
+{
+    public class CreateBookingDto
+    {
+
+
+        public string Name { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public int PersonCount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
